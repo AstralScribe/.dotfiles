de4ce7b22d50a17dc09050cb0dec1c3718acbea7
Mayank Sinha <sinha.mayank900@gmail.com>
2024-06-30 19:01:09 +0530

    bugfix

--- scripts/configure.py
+++ scripts/configure.py
@@ -13,7 +13,10 @@ def configure():
         pkg = helpers.AttrDict(config)
         print(f"\033[0;34m[Symlink]\033[0m {pkg.is_symlink} \033[0;32m[Configuring]\033[0m {name} {pkg.dst_path} <-- {pkg.src_path}.")
         if pkg.is_symlink:
-            os.symlink(src=pkg.src_path, dst=pkg.dst_path, target_is_directory=pkg.is_directory)
+            try:
+                os.symlink(src=pkg.src_path, dst=pkg.dst_path, target_is_directory=pkg.is_directory)
+            except FileExistsError:
+                print("File already exists...")
         else:
             try:
                 shutil.copytree(src=pkg.src_path, dst=pkg.dst_path, dirs_exist_ok=True)
