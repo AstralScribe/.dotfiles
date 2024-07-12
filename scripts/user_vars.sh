4c4e41411ef37d40120ae9c4b5e1e597d8ac5c24
Mayank Sinha <sinha.mayank900@gmail.com>
2024-07-12 12:13:33 +0530

    Added local bin to user vars

--- scripts/user_vars.sh
+++ scripts/user_vars.sh
@@ -5,7 +5,9 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 sysScrPath="$XDG_CONFIG_HOME/myde/sysScripts"
 custScrPath="$XDG_CONFIG_HOME/myde/custScripts"
+userLocalPath="$HOME/.local/bin"
 
+append_path "$userLocalPath"
 append_path "$sysScrPath"
 append_path "$custScrPath"
 export PATH
